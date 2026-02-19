a7cd39ebddfb56498cfcce7b1174397e469f3a14
Akintade Oluwaseun <akintadeseun816@gmail.com>
2026-02-19 01:01:14 +0000

    feat: architecture setup complete

---
+++ LEXICA/Features/Onboarding/OnboardingView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text("Onboarding")
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
---
+++ LEXICA/Features/Reading/CameraReadingView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct CameraReadingView: View {
+    var body: some View {
+        Text("Camera Reading")
+    }
+}
+
+#Preview {
+    CameraReadingView()
+}
